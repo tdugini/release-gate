c4120b5cc098f5acb67a2fa3a60450c5fafc23b3
tdugini <105417196+tdugini@users.noreply.github.com>
2026-08-27 11:29:58 +0200

    Add runtime test clients

--- tests/ReleaseGate.Api.IntegrationTests/TestClients.cs
+++ tests/ReleaseGate.Api.IntegrationTests/TestClients.cs
@@ -1,4 +1,5 @@
 using System.Net.Http.Headers;
+using ReleaseGate.Api.Security;
 
 namespace ReleaseGate.Api.IntegrationTests;
 
@@ -7,20 +8,35 @@ internal static class TestClients
     public const string OperatorToken = "releasegate-test-operator";
     public const string ReviewerToken = "releasegate-test-reviewer";
     public const string DualRoleToken = "releasegate-test-dual-role";
+    public const string RuntimeApiKey = "releasegate-test-runtime";
+    public const string ScopedRuntimeApiKey = "releasegate-test-runtime-scoped";
 
     public static HttpClient CreateOperator(ReleaseGateApiFactory factory) =>
-        Create(factory, OperatorToken);
+        CreateControlPlane(factory, OperatorToken);
 
     public static HttpClient CreateReviewer(ReleaseGateApiFactory factory) =>
-        Create(factory, ReviewerToken);
+        CreateControlPlane(factory, ReviewerToken);
 
     public static HttpClient CreateDualRole(ReleaseGateApiFactory factory) =>
-        Create(factory, DualRoleToken);
+        CreateControlPlane(factory, DualRoleToken);
 
-    private static HttpClient Create(ReleaseGateApiFactory factory, string token)
+    public static HttpClient CreateRuntime(ReleaseGateApiFactory factory) =>
+        CreateRuntime(factory, RuntimeApiKey);
+
+    public static HttpClient CreateScopedRuntime(ReleaseGateApiFactory factory) =>
+        CreateRuntime(factory, ScopedRuntimeApiKey);
+
+    private static HttpClient CreateControlPlane(ReleaseGateApiFactory factory, string token)
     {
         var client = factory.CreateClient();
         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        return client;
+    }
+
+    private static HttpClient CreateRuntime(ReleaseGateApiFactory factory, string apiKey)
+    {
+        var client = factory.CreateClient();
+        client.DefaultRequestHeaders.Add(RuntimeApiKeyValidator.HeaderName, apiKey);
         return client;
     }
 }
